71e2baa183e9141df9b801fbe8c2cc462d383d63
dannysegal18 <143559273+dannysegal18@users.noreply.github.com>
2024-01-29 19:04:52 -0800

    inclass Complete

--- m5-w4-d1-demo/demo3/on-click.js
+++ m5-w4-d1-demo/demo3/on-click.js
@@ -4,35 +4,36 @@ class OnClickElements extends React.Component {
   constructor() {
     super();
   }
-
-  //add handler
+handleClick(event) {
+  alert('hey! you clicked: ${event.target.id}')
+}
 
   render() {
     // TODO: Your task is to add onClick listeners to each of the elements within the container-div <div>
     return (
       <div className="container-div">
-        <div id="div-element" >
+        <div id="div-element" OnClickElements={(e) => this.handleClick(e)} >
           I am DIV
         </div>
 
-        <span id="span-element" >
+        <span id="span-element"OnClickElements={(e) => this.handleClick(e)}  >
           I am SPAN
         </span>
         <br></br>
 
-        <button id="button-element" >
+        <button id="button-element" OnClickElements={(e) => this.handleClick(e)} >
           I am Button
         </button>
         <br></br>
 
-        <a id="link-element" href="" >
+        <a id="link-element" href="" OnClickElements={(e) => this.handleClick(e)} >
           I am LINK
         </a>
 
         <div
           id="div-element-2"
           className="button"
-          
+          OnClickElements={(e) => this.handleClick(e)}
         >
           I am DIV
         </div>
@@ -40,7 +41,7 @@ class OnClickElements extends React.Component {
         <span
           id="span-element-2"
           className="button"
-          
+          OnClickElements={(e) => this.handleClick(e)}
         >
           I am SPAN
         </span>
@@ -49,17 +50,17 @@ class OnClickElements extends React.Component {
         <button
           id="button-element-2"
           className="button"
-          
+          OnClickElements={(e) => this.handleClick(e)}
         >
           I am Button
         </button>
         <br></br>
-
+        OnClickElements={(e) => this.handleClick(e)}
         <a
           id="link-element-2"
           className="button"
           href=""
-          
+          OnClickElements={(e) => this.handleClick(e)}
         >
           I am LINK
         </a>
